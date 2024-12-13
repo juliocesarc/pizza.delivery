ae828a49cd2cd53173450c541c088eedb3d1de51
juliocesarc <juliocostaesilva4@gmail.com>
2024-12-13 20:55:00 -0300

    env variables

--- app/(dashboard)/(routes)/products/components/client.tsx
+++ app/(dashboard)/(routes)/products/components/client.tsx
@@ -81,7 +81,7 @@ export const ProductsClient: React.FC<ProductsClientProps> = ({
           description="Gerencie seus produtos"
         />
         <Button onClick={() => router.push(`/products/new`)}>
-          <Plus className="mr-2 h-4 w-4" /> Adicionar novo
+          <Plus className="mr-2 h-4 w-4" /> Adicionar produto
         </Button>
       </div>
       <Separator />
